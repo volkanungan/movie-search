55395732d6d3dd2d77bfb0be584a43d30d4f02da
Volkan Ungan <volkan.ungan@gmail.com>
2022-12-13 09:45:38 +0100

    fix aria property

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -3,10 +3,7 @@ import SearchBar from './components/SearchBar';
 
 export default function LandingPage() {
   return (
-    <div
-      className="flex flex-col items-center gap-5 m-[3%] justify-center"
-      aria-hidden="true"
-    >
+    <div className="flex flex-col items-center gap-5 m-[3%] justify-center">
       <FaFilm
         className="text-8xl text-sizzling-red drop-shadow-xl"
         aria-hidden="true"
