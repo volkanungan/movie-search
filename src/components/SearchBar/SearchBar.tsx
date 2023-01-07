ee27ee59f2da8dfdead6d4849fe5b7ee5762a08d
Volkan Ungan <volkan.ungan@gmail.com>
2023-01-07 16:36:29 +0100

    add search suggestions

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,9 @@
-import { createSearchParams, useNavigate } from 'react-router-dom';
+import { createSearchParams, Link, useNavigate } from 'react-router-dom';
 import { BsSearch } from 'react-icons/bs';
-import { useRef, useEffect } from 'react';
+import { useRef, useState } from 'react';
 import SearchFilters from './SearchFilters';
+import { useQuery } from '@tanstack/react-query';
+import fetchSearchSuggestions from '../../lib/fetch/fetchSearchSuggestions';
 
 type ReactFormOrMouseEvent =
   | React.FormEvent<HTMLFormElement>
@@ -13,13 +15,18 @@ type SearchBarProps = {
   searchType?: string;
 };
 
+const MAX_SUGGESTIONS = 5;
+
 export default function SearchBar({
   initialSearchQuery,
   includeFilterOptions = true,
   searchType,
 }: SearchBarProps) {
   const searchText = useRef<HTMLInputElement>(null);
+  const suggestionsList = useRef<HTMLUListElement>(null);
   const navigate = useNavigate();
+  const [suggestionsQuery, setSuggestionsQuery] = useState('');
+  const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(-1);
 
   function onSubmit(e: ReactFormOrMouseEvent, videoType?: string) {
     e.preventDefault();
@@ -44,9 +51,49 @@ export default function SearchBar({
     });
   }
 
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (!suggestions.data) {
+      return;
+    }
+    if (e.key === 'ArrowUp') {
+      if (activeSuggestionIndex < 1) {
+        return;
+      }
+      setActiveSuggestionIndex((suggestionIndex) => suggestionIndex - 1);
+    } else if (e.key === 'ArrowDown') {
+      if (
+        activeSuggestionIndex ===
+        Math.min(suggestions.data.Search.length, MAX_SUGGESTIONS) - 1
+      ) {
+        return;
+      }
+      setActiveSuggestionIndex((suggestionIndex) => suggestionIndex + 1);
+    } else if (e.key === 'Enter') {
+      navigate(
+        `../movie/${suggestions.data.Search[activeSuggestionIndex].imdbID}`
+      );
+    }
+  }
+
+  let throttleId: NodeJS.Timeout;
+  function throttle(text: string) {
+    if (throttleId) {
+      clearTimeout(throttleId);
+    }
+    throttleId = setTimeout(() => {
+      setActiveSuggestionIndex(-1);
+      setSuggestionsQuery(text);
+    }, 300);
+  }
+
+  const suggestions = useQuery(
+    ['search-suggestions', suggestionsQuery],
+    fetchSearchSuggestions
+  );
+
   return (
     <div className="md:w-96">
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} autoComplete="off" className="relative">
         <div className="flex flex-row items-stretch w-full">
           <input
             type="search"
@@ -56,7 +103,36 @@ export default function SearchBar({
             name="movies"
             ref={searchText}
             defaultValue={initialSearchQuery}
+            onChange={(e) => throttle(e.target.value)}
+            onKeyDown={handleKeyDown}
           />
+          {suggestions.data?.Response == 'True' && (
+            <div className="bg-baby-powder border border-solid w-full h-34 absolute top-12 z-10 rounded shadow-lg">
+              <ul
+                className="leading-7 text-base font-alternative"
+                ref={suggestionsList}
+              >
+                {suggestions.data.Search.slice(0, MAX_SUGGESTIONS).map(
+                  (suggestion, index) => (
+                    <Link
+                      to={`../movie/${suggestion.imdbID}`}
+                      onFocus={() => setActiveSuggestionIndex(index)}
+                    >
+                      <li
+                        onMouseEnter={() => setActiveSuggestionIndex(index)}
+                        className={`px-3 ${
+                          index === activeSuggestionIndex &&
+                          `bg-middle-gray text-baby-powder text-bold`
+                        } `}
+                      >
+                        {suggestion.Title}
+                      </li>
+                    </Link>
+                  )
+                )}
+              </ul>
+            </div>
+          )}
           <button
             className="btn inline-block px-6 py-2.5 bg-black text-baby-powder font-medium text-xs leading-tight uppercase rounded-r-lg shadow-md hover:bg-bistre border-black border-solid border-2 hover:border-sizzling-red focus:border-sizzling-red hover:shadow-lg focus:bg-bistre focus:shadow-lg focus:outline-none focus:ring-0 active:bistre active:shadow-lg transition duration-150 ease-in-out items-center"
             type="submit"
@@ -66,6 +142,7 @@ export default function SearchBar({
             {/* Show text instead of icon for screen readers */}
           </button>
         </div>
+
         <>
           {includeFilterOptions && (
             <SearchFilters searchType={searchType} onFilterClick={onSubmit} />
---
+++ src/lib/fetch/fetchSearchSuggestions.ts
@@ -0,0 +1,31 @@
+import { QueryFunction } from '@tanstack/react-query';
+import { MovieSearchAPIResponse } from '../APIResponsesTypes';
+const API_KEY = '1672fa38';
+
+const fetchSearchSuggestions: QueryFunction<
+  MovieSearchAPIResponse,
+  ['search-suggestions', string]
+> = async ({ queryKey }) => {
+  const title = queryKey[1];
+
+  if (!title || title.length === 0) {
+    throw new Error('Seach parameter "title" cannot be null');
+  }
+
+  if (title.length < 3) {
+    return [];
+  }
+
+  const apiRes = await fetch(
+    `https://www.omdbapi.com/?s=${title}&apikey=${API_KEY}`
+  );
+
+  if (!apiRes.ok) {
+    throw new Error(
+      `https://www.omdbapi.com/?s=${title}&apikey=${API_KEY} fetch failed`
+    );
+  }
+
+  return apiRes.json();
+};
+export default fetchSearchSuggestions;
